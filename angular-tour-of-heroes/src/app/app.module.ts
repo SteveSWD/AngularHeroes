0f7b9f27605dd22d2f72bad5127f77eaba5f080f
schmidsm <steve.schmidt@wolfdenassociates.com>
2018-03-16 13:14:46 -0400

    Added Hero Service for data

--- angular-tour-of-heroes/src/app/app.module.ts
+++ angular-tour-of-heroes/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { HeroesComponent } from './heroes/heroes.component';
 
 import { FormsModule } from '@angular/forms';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { HeroService } from './hero.service';
 
 @NgModule({
   declarations: [
@@ -18,7 +19,10 @@ import { HeroDetailComponent } from './hero-detail/hero-detail.component';
     BrowserModule,
     FormsModule
   ],
-  providers: [],
+  providers: [
+    HeroService,
+    /*...*/
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
