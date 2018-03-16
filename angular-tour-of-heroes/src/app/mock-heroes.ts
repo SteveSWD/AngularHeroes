89ede4697d8282edd089e058dae8d852bf412616
schmidsm <steve.schmidt@wolfdenassociates.com>
2018-03-16 12:09:24 -0400

    Initial project commit. Hero and Hero-detail components created

---
+++ angular-tour-of-heroes/src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Steve' },
+  { id: 12, name: 'Tom' },
+  { id: 13, name: 'Jared' },
+  { id: 14, name: 'Brandy' },
+  { id: 15, name: 'Sue' },
+  { id: 16, name: 'Jamie' },
+  { id: 17, name: 'Sam' },
+  { id: 18, name: 'Jessica' },
+  { id: 19, name: 'Greg' },
+  { id: 20, name: 'Alex' }
+];
